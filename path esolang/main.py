032e0e68020d6a067531c5ab2bd5b792e6637b3d
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-09-23 15:03:36 +0300

    Update main.py

--- path esolang/main.py
+++ path esolang/main.py
@@ -1,7 +1,8 @@
 """Path is an esolang created by Gaham where multiple brackets are used as a part of a code
 (...) pushes the length of the contents surrounded by curly brackets
 [...] comments
-<...> manages the program execution state"""
+<...> manages the program execution state
+{...} execute a part of a program"""
 m="";s=[];l=[]
 def code(i):
   global m
@@ -17,6 +18,9 @@ def code(i):
     elif c=="[" and m=="Y":m="i"
     elif c=="]" and m=="i":m="Y"
     elif m=="i":print(end="")
+    elif c=="{" and m=="Y":m="c"
+    elif c=="}" and m=="c":m="Y"
+    elif c=="+" and m=="c":a=int(s[0])+int(s[1]);s.reverse();s.pop(0);s.pop(0);s.append(a);s.reverse()
     else:print("UNKNOWN COMMAND. HALT");break
   print(f"stack:{s}")
 code("")
