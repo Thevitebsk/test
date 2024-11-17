77b1ed7e17a63fc2908910d197902dd4eae9cf20
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-17 21:31:14 +0300

    it's over

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("i am not going to write the basic program becuase yes >:)")
+import sqlite3
+connection=sqlite3.connect("3wr.db")
+cursor=connection.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS weather (
+    t INTEGER NOT NULL,
+    w TEXT NOT NULL
+);''')
+connection.commit()
+cursor.execute("INSERT INTO weather (t, w) VALUES (3, 'tuesday');")
+cursor.execute("INSERT INTO weather (t, w) VALUES (5, 'wensday');")
+cursor.execute("INSERT INTO weather (t, w) VALUES (1, 'thirsday');")
+cursor.execute("INSERT INTO weather (t, w) VALUES (0, 'friday');")
+cursor.execute("INSERT INTO weather (t, w) VALUES (-1, 'saturday');")
+cursor.execute("INSERT INTO weather (t, w) VALUES (0, 'sunday');")
+cursor.execute("INSERT INTO weather (t, w) VALUES (5, 'monday');")
+connection.commit()
+connection.close()
